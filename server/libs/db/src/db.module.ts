338ec04d832d62e0aeb84d9199a11717425704c2
unknown <1035549128@qq.com>
2020-08-24 17:23:53 +0800

    一

---
+++ server/libs/db/src/db.module.ts
@@ -0,0 +1,23 @@
+import { Module,Global } from '@nestjs/common';
+import { DbService } from './db.service';
+import { TypegooseModule } from 'nestjs-typegoose'
+import {User} from './models/user.model'
+
+
+const models = TypegooseModule.forFeature([User])
+
+@Global()
+@Module({
+  imports:[
+    TypegooseModule.forRoot('mongodb://localhost/topfullstack', {
+      useNewUrlParser : true,
+      useCreateIndex : true,
+      useUnifiedTopology : true,
+      useFindAndModify : false
+    }),
+    models,
+  ],
+  providers: [DbService],
+  exports: [DbService,models],
+})
+export class DbModule {}
---
+++ server/libs/db/src/models/user.model.ts
@@ -0,0 +1,12 @@
+import { prop } from '@typegoose/typegoose'
+
+
+export class User {
+    @prop()
+
+    username:string
+
+    @prop()
+
+    password:string
+}
